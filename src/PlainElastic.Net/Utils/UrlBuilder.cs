30be1350a106188857636e4161be338055d325e7
Yegoroff <AlexYegoroff+github@gmail.com>
2012-01-11 22:32:04 +0200

    Added UrlBuilder utility to build and modify URLs

---
+++ src/PlainElastic.Net/Utils/UrlBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlainElastic.Net
+{
+    public class UrlBuilder
+    {
+
+        public static string BuildUrlPath(params string[] urlParts)
+        {
+            string path = urlParts.Where(p => !p.IsNullOrEmpty()).JoinWithSeparator("/");
+            return path;
+        }
+
+        public static string AddParameter(string url, string paramName, string paramValue)
+        {
+             if (url.Contains('?'))
+                 return "{0}&{1}={2}".F(url, paramName, paramValue);
+
+             return "{0}?{1}={2}".F(url, paramName, paramValue);           
+        }
+
+    }
+}

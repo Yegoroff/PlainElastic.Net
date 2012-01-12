f71b1938da08b63afd580cdb13300a8ed2b41426
Yegoroff <AlexYegoroff+github@gmail.com>
2012-01-12 03:57:24 +0200

    Added comments to PlainSample

--- src/Samples/PlainSample/Program.cs
+++ src/Samples/PlainSample/Program.cs
@@ -11,27 +11,30 @@ namespace PlainSample
             var connection = new ElasticConnection {DefaultHost = "localhost", DefaultPort = 9200};
             var serializer = new JsonNetSerializer();
 
-           
-            IndexTweet(connection, serializer);
+            var tweet = new Tweet
+            {
+                User = "testUser",
+                Message = "Test Message"
+            };
+
+            IndexTweet(tweet,connection, serializer);
 
             GetTweet(serializer, connection);
 
             DeleteTweet(connection);
 
-
             Console.ReadKey();
         }
 
 
-        private static void IndexTweet(ElasticConnection connection, JsonNetSerializer serializer)
+        private static void IndexTweet(Tweet tweet, ElasticConnection connection, JsonNetSerializer serializer)
         {
-            var tweet = new Tweet
-                            {
-                                User = "testUser",
-                                Message = "Test Message"
-                            };
+            
+            // This is url that will be requested from ES. We can grab it and put to any ES admin console (like Head) to debug ES behavior.
+            string indexCommand = new IndexCommandBuilder(index: "tweeter", type: "tweet", id: "1").Pretty();          
 
-            string indexCommand = new IndexCommandBuilder(index: "tweeter", type: "tweet", id: "1").Pretty();
+            // This variable contains JSON of serialized tweet, thus we can check if our object serialized correctly 
+            // or use it directly in ES admin console.
             string data = serializer.ToJson(tweet);
 
             var result = connection.Put(indexCommand, data);
@@ -39,10 +42,15 @@ namespace PlainSample
             var indexResult = serializer.ToIndexResult(result);
 
 
-            Console.WriteLine("Index RESULTS \r\n" +
+            Console.WriteLine("Executed: PUT \r\n" +
+                  indexCommand + "\r\n" +
+                  data +
+                  "\r\n");
+
+            Console.WriteLine("Index Results \r\n" +
                               result +
                               "\r\n");
-
+            Console.WriteLine("Parsed Index Results");
             Console.WriteLine("ok: " + indexResult.ok);
             Console.WriteLine("_index: " + indexResult._index);
             Console.WriteLine("_type: " + indexResult._type);
@@ -50,8 +58,8 @@ namespace PlainSample
             Console.WriteLine("_version: " + indexResult._version);
             Console.WriteLine();
         }
-
-        private static void GetTweet(JsonNetSerializer serializer, ElasticConnection connection)
+        
+        private static Tweet GetTweet(JsonNetSerializer serializer, ElasticConnection connection)
         {
             String getCommand = new GetCommandBuilder(index: "tweeter", type: "tweet", id: "1").Pretty();
 
@@ -60,13 +68,19 @@ namespace PlainSample
             var getTweet = serializer.ToGetResult<Tweet>(result);
 
 
-            Console.WriteLine("Get RESULT: \r\n" +
-                              result +
+            Console.WriteLine("Executed: GET \r\n" +
+                              getCommand + 
                               "\r\n");
 
+            Console.WriteLine("Get Result: \r\n" +
+                              result +
+                              "\r\n");
+            Console.WriteLine("Parsed Get Result: ");
             Console.WriteLine("User: " + getTweet.User);
             Console.WriteLine("Message: " + getTweet.Message);
             Console.WriteLine();
+
+            return getTweet;
         }
 
         private static void DeleteTweet(ElasticConnection connection)

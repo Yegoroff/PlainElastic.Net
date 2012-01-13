7d455d9d4a42f01f67657b9f563d56133fc47ab9
Yegoroff <AlexYegoroff+github@gmail.com>
2012-01-13 12:22:25 +0200

    Added DeleteCommandBuilder

---
+++ src/PlainElastic.Net/Commands/DeleteCommandBuilder.cs
@@ -0,0 +1,69 @@
+﻿namespace PlainElastic.Net
+{
+    /// <summary>
+    /// Builds a command that allows to delete custom Json document in Index or delete whole Index.
+    /// </summary>
+    public class DeleteCommandBuilder: CommandBuilder<DeleteCommandBuilder>
+    {
+        public string Index { get; private set; }
+
+        public string Type { get; private set; }
+
+        public string Id { get; private set; }
+
+
+        public DeleteCommandBuilder(string index, string type = null, string id = null)
+        {
+            Index = index;
+            Type = type;
+            Id = id;
+        }
+
+
+        #region Query Parameters
+
+        public DeleteCommandBuilder Consistency(WriteConsistency consistency)
+        {
+            Parameters.Add("consistency", consistency.ToString());
+            return this;
+        }
+
+        public DeleteCommandBuilder Parent(string parentId)
+        {
+            Parameters.Add("parent", parentId);
+            return this;
+        }
+
+        public DeleteCommandBuilder Refresh(bool refresh)
+        {
+            Parameters.Add("refresh", refresh.AsString());
+            return this;
+        }
+
+        public DeleteCommandBuilder Replication(DocumentReplication replication)
+        {
+            Parameters.Add("replication", replication.ToString());
+            return this;
+        }
+
+        public DeleteCommandBuilder Routing(string routing)
+        {
+            Parameters.Add("routing", routing);
+            return this;
+        }
+
+        public DeleteCommandBuilder Version(long version)
+        {
+            Parameters.Add("version", version.ToString());
+            return this;
+        }
+
+        #endregion
+
+
+        protected override string BuildUrlPath()
+        {
+            return UrlBuilder.BuildUrlPath(Index, Type, Id);
+        }
+    }
+}

a06afaf014d0dbb6658f88ca04a713070dea4e83
Yegoroff <AlexYegoroff+github@gmail.com>
2012-01-18 02:21:43 +0200

    Added SearchCommand call to PlainSample.

--- src/Samples/PlainSample/Program.cs
+++ src/Samples/PlainSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using PlainElastic.Net;
 using PlainElastic.Net.Serialization;
 
@@ -17,9 +18,19 @@ namespace PlainSample
                 Message = "trying out Elastic Search"
             };
 
+            var anotherTweet = new Tweet
+            {
+                User = "anotherUser",
+                Message = "one more message"
+            };
+
             IndexTweet(tweet, "1", connection, serializer);
 
+            IndexTweet(anotherTweet, "2", connection, serializer);
+
             GetTweet("1", serializer, connection);
+
+            SearchTweets(connection, serializer);
 
             DeleteTweeterIndex(connection, serializer);
 
@@ -37,7 +48,10 @@ namespace PlainSample
            */
 
             // This is url that will be requested from ES. We can grab it and put to any ES admin console (like Head) to debug ES behavior.
-            string indexCommand = new IndexCommandBuilder(index: "twitter", type: "tweet", id: id).Pretty(); // this will generate: twitter/tweet/1?pretty=true
+            string indexCommand = new IndexCommandBuilder(index: "twitter", type: "tweet", id: id)
+               // .Consistency(WriteConsistency.all)
+                .Refresh(true)
+                .Pretty(); // this will generate: twitter/tweet/1?pretty=true
 
             // This variable contains JSON of serialized tweet, thus we can check if our object serialized correctly 
             // or use it directly in ES admin console.
@@ -49,24 +63,24 @@ namespace PlainSample
             IndexResult indexResult = serializer.ToIndexResult(result);
 
 
+            PrintIndexCommand(result, indexResult, indexCommand, data);
+        }
 
-            Console.WriteLine("Executed: PUT \r\n" +
-                  indexCommand + "\r\n" +
-                  data +
-                  "\r\n");
+        private static void PrintIndexCommand(OperationResult result, IndexResult indexResult, string indexCommand, string data)
+        {
+            Console.WriteLine("Executed: PUT \r\n{0} \r\n{1} \r\n".F(indexCommand, data ));
 
-            Console.WriteLine("Index Results \r\n" +
-                              result +
-                              "\r\n");
+            Console.WriteLine("Index Results \r\n{0} \r\n".F(result));
 
             Console.WriteLine("Parsed Index Results");
-            Console.WriteLine("ok: "     + indexResult.ok);
-            Console.WriteLine("_index: " + indexResult._index);
-            Console.WriteLine("_type: "  + indexResult._type);
-            Console.WriteLine("_id: "    + indexResult._id);
-            Console.WriteLine("_version: " + indexResult._version);
+            Console.WriteLine(" ok: " + indexResult.ok);
+            Console.WriteLine(" _index: " + indexResult._index);
+            Console.WriteLine(" _type: " + indexResult._type);
+            Console.WriteLine(" _id: " + indexResult._id);
+            Console.WriteLine(" _version: " + indexResult._version);
             Console.WriteLine();
         }
+
 
         private static Tweet GetTweet(string id, JsonNetSerializer serializer, ElasticConnection connection)
         {
@@ -78,24 +92,28 @@ namespace PlainSample
 
             var result = connection.Get(getCommand); 
 
-            // Deserialize Get command result to Tweet object.
-            var getTweet = serializer.ToGetResult<Tweet>(result);
+            // Deserialize Get command result to GetResult object.
+            var getResult = serializer.ToGetResult<Tweet>(result);
 
+            var getTweet = getResult.Document;
 
-            Console.WriteLine("Executed: GET \r\n" +
-                              getCommand + 
-                              "\r\n");
-
-            Console.WriteLine("Get Result: \r\n" +
-                              result +
-                              "\r\n");
-            Console.WriteLine("Parsed Get Result: ");
-            Console.WriteLine("User: " + getTweet.User);
-            Console.WriteLine("Message: " + getTweet.Message);
-            Console.WriteLine();
+            PrintGetCommand(getTweet, result, getCommand);
 
             return getTweet;
         }
+
+        private static void PrintGetCommand(Tweet getTweet, OperationResult result, string getCommand)
+        {
+            Console.WriteLine("Executed: GET \r\n {0} \r\n".F(getCommand));
+
+            Console.WriteLine("Get Result: \r\n {0} \r\n".F(result));
+
+            Console.WriteLine("Parsed Get Result: ");
+            Console.WriteLine(" User: " + getTweet.User);
+            Console.WriteLine(" Message: " + getTweet.Message);
+            Console.WriteLine();
+        }
+
 
         private static void DeleteTweeterIndex(ElasticConnection connection, JsonNetSerializer serializer)
         {
@@ -111,19 +129,73 @@ namespace PlainSample
 
 
 
-            Console.WriteLine("Delete RESULT: \r\n" + result);
+            PrintDeleteCommand(deleteCommand, deleteResult, result);
+        }
+
+        private static void PrintDeleteCommand(string deleteCommand, DeleteResult deleteResult, OperationResult result)
+        {
+            Console.WriteLine("Executed: DELETE \r\n {0} \r\n".F(deleteCommand));
+
+            Console.WriteLine("Delete RESULT: \r\n{0} \r\n".F(result));
 
             Console.WriteLine("Parsed Delete Results");
-            Console.WriteLine("ok: " + deleteResult.ok);
-            Console.WriteLine("_index: " + deleteResult._index);
-            Console.WriteLine("_type: " + deleteResult._type);
-            Console.WriteLine("_id: " + deleteResult._id);
-            Console.WriteLine("found: " + deleteResult.found);
+            Console.WriteLine(" ok: " + deleteResult.ok);
+            Console.WriteLine(" _index: " + deleteResult._index);
+            Console.WriteLine(" _type: " + deleteResult._type);
+            Console.WriteLine(" _id: " + deleteResult._id);
+            Console.WriteLine(" found: " + deleteResult.found);
             Console.WriteLine();
-
         }
-    
-    }
+
+
+        private static IEnumerable<Tweet> SearchTweets(ElasticConnection connection, JsonNetSerializer serializer)
+        {
+            string searchCommand = ElasticCommands.Search("twitter", "tweet").Pretty();
+
+            var results = connection.Post(searchCommand, "{}");
+
+            var searchResult = serializer.ToSearchResult<Tweet>(results);
+
+
+            PrintSearchResults(searchResult, searchCommand, results);
+
+            return searchResult.Documents;
+        }
+
+        private static void PrintSearchResults(SearchResult<Tweet> searchResult, string searchCommand, OperationResult results)
+        {
+            Console.WriteLine("Executed: GET \r\n {0} \r\n".F(searchCommand));
+
+            Console.WriteLine("Search Result: \r\n {0} \r\n".F(results));
+
+            Console.WriteLine("Parsed Search Results");
+            Console.WriteLine(" took: " + searchResult.took);
+            Console.WriteLine(" timed_out: " + searchResult.timed_out);
+            Console.WriteLine(" _shards:");
+            Console.WriteLine("     total: " + searchResult._shards.total);
+            Console.WriteLine("     successful: " + searchResult._shards.successful);
+            Console.WriteLine("     failed: " + searchResult._shards.failed);
+
+            Console.WriteLine(" hits: ");
+            Console.WriteLine("     total: " + searchResult.hits.total);
+            Console.WriteLine("     max_score: " + searchResult.hits.max_score);
+            Console.WriteLine("     hits: ");
+            foreach (var hit in searchResult.hits.hits)
+            {
+                Console.WriteLine("         _id: " + hit._id);
+                Console.WriteLine("         _index: " + hit._index);
+                Console.WriteLine("         _type: " + hit._type);
+                Console.WriteLine("         _score: " + hit._score);
+                Console.WriteLine("         _source:");
+
+                Console.WriteLine("             User: " + hit._source.User);
+                Console.WriteLine("             Message: " + hit._source.Message);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+     }
 
 
     public class Tweet

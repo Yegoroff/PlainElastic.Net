6c08e493be4b851055591ffe94e4e9862c153341
Yegoroff <AlexYegoroff+github@gmail.com>
2012-01-18 02:40:46 +0200

    Added initial NuGet support

---
+++ NuGet Publish.bat
@@ -0,0 +1,16 @@
+@set NUGET="packages\NuGet.CommandLine.1.6.0\tools\NuGet.exe"
+
+@echo ==========================
+@echo NuGet package publishing.
+@%NUGET% Push NuGet\PlainElastic.Net.1.0.0.nupkg
+@if not errorlevel 0 goto error
+
+@echo PlainElastic.Net publishing sucessfull.
+@goto end
+
+:error
+@echo Error occured during PlainElastic.Net publishing.
+@pause
+
+:end
+@pause
---
+++ NuGet.bat
@@ -0,0 +1,34 @@
+@set DEVENV100="%programfiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
+@if "%programfiles(x86)%"=="" (@set DEVENV100="%programfiles%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe")
+
+@set NUGET="packages\NuGet.CommandLine.1.6.0\tools\NuGet.exe"
+
+@echo ==========================
+@echo Building PlainElastic.Net.
+@rmdir src\PlainElastic.Net\bin /s /q
+%DEVENV100% /nologo /build Release "PlainElastic.Net.sln"
+@if errorlevel 1 goto error
+
+@echo ==========================
+@echo Copying PlainElastic.Net assemblies.
+@rmdir NuGet\lib /s /q
+@mkdir NuGet\lib
+xcopy src\PlainElastic.Net\bin\Release NuGet\lib /s /y
+@if errorlevel 0 goto nuget
+@goto error
+
+:nuget
+@echo ==========================
+@echo NuGet package creation.
+@%NUGET% pack NuGet\plainelastic.net.nuspec -basePath NuGet -o NuGet
+@if not errorlevel 0 goto error
+
+@echo PlainElastic.Net build sucessfull.
+@goto end
+
+:error
+@echo Error occured during PlainElastic.Net build.
+@pause
+
+:end
+@pause

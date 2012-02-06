32e6ab9f00df70ef749dc7ad8175ece6b69b11e2
Yegoroff <AlexYegoroff+github@gmail.com>
2012-02-07 01:53:39 +0200

    Added Custom query support in Query object.

--- src/PlainElastic.Net/QueryBuilder/Query.cs
+++ src/PlainElastic.Net/QueryBuilder/Query.cs
@@ -1,15 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using PlainElastic.Net.Utils;
 
 namespace PlainElastic.Net.QueryBuilder
 {
-    public class Query<T> : AbstractQuery<T>
+    public class Query<T> : AbstractCompositeQuery<T>
     {
-
-        #region Query Templates
-
-        private const string queryTemplate = @" ""query"": {{ {0} }}";
-
-        #endregion
+        private const string queryTemplate = " \"query\": {0} ";
 
 
         protected override string QueryTemplate
@@ -20,33 +18,47 @@ namespace PlainElastic.Net.QueryBuilder
 
         public Query<T> Bool(Func<BoolQuery<T>, BoolQuery<T>> boolQuery)
         {
-            ExecuteAndRegisterQuery(boolQuery);
+            RegisterQueryAsJson(boolQuery);
             return this;
         }
 
         public Query<T> QueryString(Func<QueryString<T>, QueryString<T>> queryString)
         {
-            ExecuteAndRegisterQuery(queryString);
+            RegisterQueryAsJson(queryString);
             return this;
         }
 
         public Query<T> Term(Func<Term<T>, Term<T>> termQuery)
         {
-            ExecuteAndRegisterQuery(termQuery);
+            RegisterQueryAsJson(termQuery);
             return this;
         }
 
         public Query<T> Terms(Func<Terms<T>, Terms<T>> termsQuery)
         {
-            ExecuteAndRegisterQuery(termsQuery);
+            RegisterQueryAsJson(termsQuery);
             return this;
         }
 
-        public Query<T> Custom(string query)
+        public Query<T> Custom(string queryFormat, Expression<Func<T, object>> field, params string[] args)
         {
+            var formatArgs = new List<string>(args);
+            formatArgs.Insert(0, field.GetPropertyName());
+
+            return Custom(queryFormat, formatArgs.ToArray());
+        }
+
+
+        public Query<T> Custom(string queryFormat, params string[] args)
+        {
+            queryFormat = queryFormat.Replace('\'', '\"');
+
+            var query = queryFormat.F(args);
             Queries.Add(query);
             return this;
-        } 
+        }
+
+
 
     }
 }

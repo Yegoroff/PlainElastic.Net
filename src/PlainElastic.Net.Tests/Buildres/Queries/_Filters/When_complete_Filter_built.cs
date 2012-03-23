caea1239d8236604ca9d85ee6160088251fd36d3
Yegoroff <AlexYegoroff+github@gmail.com>
2012-03-23 03:11:28 +0200

    Cover filter builders with tests.

---
+++ src/PlainElastic.Net.Tests/Buildres/Queries/_Filters/When_complete_Filter_built.cs
@@ -0,0 +1,26 @@
+﻿using Machine.Specifications;
+using PlainElastic.Net.Queries;
+using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net.Tests.Buildres.Queries
+{
+    [Subject(typeof(Filter<>))]
+    class When_complete_Filter_built
+    {
+        Because of = () => result = new Filter<FieldsTestClass>()                                                
+                                            .And( a => a.Custom("And"))
+                                            .Exists(e => e.Custom("Exists"))
+                                            .Nested(n => n.Custom("Nested"))
+                                            .Range(r => r.Custom("Range"))
+                                            .Term(t => t.Custom("Term"))
+                                            .Terms(t => t.Custom("Terms"))
+                                            .ToString();
+
+        It should_starts_with_nested_declaration = () => result.ShouldStartWith("'filter': {".AltQuote());
+
+
+        It should_return_correct_result = () => result.ShouldEqual("'filter': { 'and': [ And ] },{ 'exists': { Exists } },{ 'nested': { Nested } },{ 'range': { Range } },{ 'term': { Term } },{ 'terms': { Terms } }".AltQuote());
+
+        private static string result;
+    }
+}

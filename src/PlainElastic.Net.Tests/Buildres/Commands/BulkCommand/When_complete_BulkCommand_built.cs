774b290fd2e997ed118fd6511017a252a497a4cf
Yegoroff <AlexYegoroff+github@gmail.com>
2012-04-01 02:00:16 +0300

    Added Replication parameter to BulkCommand

--- src/PlainElastic.Net.Tests/Buildres/Commands/BulkCommand/When_complete_BulkCommand_built.cs
+++ src/PlainElastic.Net.Tests/Buildres/Commands/BulkCommand/When_complete_BulkCommand_built.cs
@@ -8,6 +8,7 @@ namespace PlainElastic.Net.Tests.Buildres.Commands
 
         Because of = () => result = new BulkCommand(index: "Index", type: "Type")
             .Consistency(WriteConsistency.quorum)
+            .Replication(DocumentReplication.async)
             .Refresh(true)
             .BuildCommand();
 
@@ -18,9 +19,11 @@ namespace PlainElastic.Net.Tests.Buildres.Commands
 
         It should_contain_parameter_consistency_equals_to_analyzer = () => result.ShouldContain("?consistency=quorum");
 
+        It should_contain_parameter_replication_equals_to_async = () => result.ShouldContain("&replication=async");
+
         It should_contain_parameter_refresh_equals_to_true = () => result.ShouldContain("&refresh=true");
 
-        It should_return_correct_value = () => result.ShouldEqual("index/type/_bulk?consistency=quorum&refresh=true");
+        It should_return_correct_value = () => result.ShouldEqual("index/type/_bulk?consistency=quorum&replication=async&refresh=true");
 
 
         private static string result;
--- src/PlainElastic.Net/Builders/Commands/BulkCommand.cs
+++ src/PlainElastic.Net/Builders/Commands/BulkCommand.cs
@@ -34,6 +34,20 @@ namespace PlainElastic.Net
         }
 
         /// <summary>
+        /// Allows to control Index operation replication.
+        /// By default, the index operation only returns after all shards within the replication group have indexed the document (sync replication).
+        /// To enable asynchronous replication, causing the replication process to take place in the background, 
+        /// set the replication parameter to async. 
+        /// When asynchronous replication is used, the index operation will return as soon as the operation succeeds on the primary shard.
+        /// </summary>
+        public BulkCommand Replication(DocumentReplication replication)
+        {
+            Parameters.Add("replication", replication.ToString());
+            return this;
+        }
+
+
+        /// <summary>
         /// Allows to refresh the relevant shards immediately after the bulk operation has occurred and make it searchable,
         /// instead of waiting for the normal refresh interval to expire.
         /// Setting it to true can trigger additional load, and may slow down indexing.

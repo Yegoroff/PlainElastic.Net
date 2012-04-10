6a5a93a8ab34c312420e5d1e15a47078d39a2a3f
Yegoroff <AlexYegoroff+github@gmail.com>
2012-04-10 19:10:39 +0300

    Changed version to 1.0.18

--- NuGet Publish.bat
+++ NuGet Publish.bat
@@ -2,7 +2,7 @@
 
 @echo ==========================
 @echo NuGet package publishing.
-@%NUGET% Push NuGet\PlainElastic.Net.1.0.17.nupkg
+@%NUGET% Push NuGet\PlainElastic.Net.1.0.18.nupkg
 @if not errorlevel 0 goto error
 
 @echo PlainElastic.Net publishing sucessfull.

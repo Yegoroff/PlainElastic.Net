2ad7d8f2370b1be4f987061b391aff8a91a38bdc
Andriy Kozachuk <dev.oyster@gmail.com>
2012-04-17 21:09:17 +0300

    Fixed sort to allow explicitly setting "asc" or "desc" direction.
    elasticsearch sort direction defaults not to "desc" but to field defaults (desc for "_score", asc for others) and also depends on field analyzer. Fixed for QueryBuilder<T>.Sort() and SearchCommand.Sort()

--- src/PlainElastic.Net.Tests/Buildres/Commands/SearchCommand/When_SearchCommand_with_reflected_sort_fields_built.cs
+++ src/PlainElastic.Net.Tests/Buildres/Commands/SearchCommand/When_SearchCommand_with_reflected_sort_fields_built.cs
@@ -7,15 +7,15 @@ namespace PlainElastic.Net.Tests.Buildres.Commands
     {
 
         Because of = () => result = new SearchCommand(index:"Index", type:"Type")
-            .Sort<FieldsTestClass>(c => c.StringProperty, SortDirection.ask)
+            .Sort<FieldsTestClass>(c => c.StringProperty, SortDirection.asc)
             .BuildCommand();
 
 
         It should_starts_with_index_type = () => result.ShouldStartWith("index/type");
 
-        It should_contain_first_parameter_sort_with_StringProperty_ask = () => result.ShouldContain("?sort=StringProperty:ask");
+        It should_contain_first_parameter_sort_with_StringProperty_ask = () => result.ShouldContain("?sort=StringProperty:asc");
 
-        It should_return_correct_value = () => result.ShouldEqual(@"index/type/_search?sort=StringProperty:ask");
+        It should_return_correct_value = () => result.ShouldEqual(@"index/type/_search?sort=StringProperty:asc");
 
 
         private static string result;

75cd737ecb00770052d6d6946efa59556ecc7f74
Yegoroff <AlexYegoroff+github@gmail.com>
2012-04-19 04:15:00 +0300

    Changed version to 1.0.20

--- NuGet Publish.bat
+++ NuGet Publish.bat
@@ -2,7 +2,7 @@
 
 @echo ==========================
 @echo NuGet package publishing.
-@%NUGET% Push NuGet\PlainElastic.Net.1.0.19.nupkg
+@%NUGET% Push NuGet\PlainElastic.Net.1.0.20.nupkg
 @if not errorlevel 0 goto error
 
 @echo PlainElastic.Net publishing sucessfull.

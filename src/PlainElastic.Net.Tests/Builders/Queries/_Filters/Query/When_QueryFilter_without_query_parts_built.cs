4ef8f9243b261e5a569b03016312cce86b35b5ec
Andriy Kozachuk <dev.oyster@gmail.com>
2012-04-28 18:00:41 +0300

    Added _name, _cache, _cache_key support for fquery filter builder
    Signed-off-by: Yegoroff <AlexYegoroff+github@gmail.com>

---
+++ src/PlainElastic.Net.Tests/Builders/Queries/_Filters/Query/When_QueryFilter_without_query_parts_built.cs
@@ -0,0 +1,17 @@
+﻿using Machine.Specifications;
+using PlainElastic.Net.Queries;
+
+namespace PlainElastic.Net.Tests.Builders.Queries
+{
+    [Subject(typeof(QueryFilter<>))]
+    class When_QueryFilter_without_Query_parts_built
+    {
+        Because of = () => result = new QueryFilter<FieldsTestClass>()
+                                                .Name("FilterName")
+                                                .ToString();
+
+        It should_return_empty_query = () => result.ShouldEqual("");
+
+        private static string result;
+    }
+}

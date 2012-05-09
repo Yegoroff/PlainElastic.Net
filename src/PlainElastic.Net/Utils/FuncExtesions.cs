46d3a08dff825ca192c8ba73dc973b8f9fe647f4
Yegoroff <AlexYegoroff+github@gmail.com>
2012-05-10 00:36:43 +0300

    Move AnalysisExtension to FuncExtensions and JsonHelper utility classes. This better reflects their common (not specific to Analysis) nature.

---
+++ src/PlainElastic.Net/Utils/FuncExtesions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlainElastic.Net.Utils
+{
+    public static class FuncExtesions
+    {
+        /// <summary>
+        /// Represents Monadic Bind operator.
+        /// Provides an easy way to compose functions.
+        /// Internally it passes current expression result to <paramref name="func"/> function, 
+        /// thus it could be used to apply additional logic to current expression.
+        /// </summary>
+        public static Func<T, T> Bind<T>(this Func<T, T> source, Func<T, T> func)
+        {
+            if (source == null)
+                return func;
+
+            return o => func(source(o));
+        }
+
+    }
+}
---
+++ src/PlainElastic.Net/Utils/JsonHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PlainElastic.Net.Utils
+{
+    public static class JsonHelper
+    {
+
+        public static string BuildJsonStringsProperty(string name, IEnumerable<string> values)
+        {
+            var valuesJson = values.Where(v => !v.IsNullOrEmpty()).Quotate().JoinWithComma();
+            return "{0}: [ {1} ]".F(name.Quotate(), valuesJson);
+        }
+
+    }
+}

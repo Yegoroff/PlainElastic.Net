36baf9c0a9ef4b484df7c1e94c2bb92c720c7d55
Yegoroff <AlexYegoroff+github@gmail.com>
2012-05-10 00:48:33 +0300

    Rename StopAnalyzerBase to StopwordsAnalyzerBase to better reflect it's responsibility.

---
+++ src/PlainElastic.Net/Builders/IndexSettings/Analysis/Analyzers/StopwordsAnalyzerBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net.IndexSettings
+{
+    public abstract class StopwordsAnalyzerBase<TPart> : AnalyzerBase<TPart> where TPart : StopwordsAnalyzerBase<TPart>
+    {
+        /// <summary>
+        /// Sets a list of stopwords to initialize the stop filter with.
+        /// Defaults to the language stop words.
+        /// </summary>
+        public TPart Stopwords(IEnumerable<string> stopwords)
+        {
+            string propertyJson = JsonHelper.BuildJsonStringsProperty("stopwords", stopwords);
+            RegisterJsonPart(propertyJson);
+            return (TPart)this;
+        }
+
+        /// <summary>
+        /// Sets a list of stopwords to initialize the stop filter with.
+        /// Defaults to the language stop words.
+        /// </summary>
+        public TPart Stopwords(params string[] stopwords)
+        {
+            return Stopwords((IEnumerable<string>)stopwords);
+        }
+
+
+        /// <summary>
+        /// Sets a path (either relative to config location, or absolute) to a stopwords file configuration.
+        /// </summary>
+        public TPart StopwordsPath(string stopwordsPath)
+        {
+            RegisterJsonPart("'stopwords_path': {0}", stopwordsPath.Quotate());
+            return (TPart)this;
+        }
+    }
+}

efa920c4af3b5fefbcd982e5d01a78e1163b7c9e
Yegoroff <AlexYegoroff+github@gmail.com>
2012-05-10 00:47:22 +0300

    Rename AnalysisComponentBase to NamedComponentBase, also publish ComponentName property.

---
+++ src/PlainElastic.Net/Builders/IndexSettings/Analysis/NamedComponentBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net.IndexSettings
+{
+    /// <summary>
+    /// Base class for Analyzers, Tokenizers and Filters.
+    /// </summary>
+    public abstract class NamedComponentBase<TPart> : AnalysisBase<TPart> where TPart : NamedComponentBase<TPart>
+    {
+        public string ComponentName { get; set; }
+
+        
+        public TPart Name(string name)
+        {
+            ComponentName = name;
+            return (TPart)this;
+        }
+
+        /// <summary>
+        /// Controls which Lucene version behavior this component should use.
+        /// The highest version number is the default option.
+        /// </summary>
+        public TPart Version(string version)
+        {
+            RegisterJsonPart("'version': {0}", version.Quotate());
+            return (TPart)this;
+        }
+
+
+        protected abstract string GetComponentType();
+
+
+        protected override string ApplyJsonTemplate(string body)
+        {
+            var type = GetComponentType();
+            var typeProperty = "'type': {0}".AltQuoteF(type.Quotate());
+
+            var typedBody = body.IsNullOrEmpty() ? typeProperty
+                                                 : new[] { typeProperty, body }.JoinWithComma();
+
+            return "{0}: {{ {1} }}".F(ComponentName.Quotate(), typedBody);
+        }
+
+    }
+}

829533804ae054bf8b724c7fa6e20d9e56f7e79d
Frank Hsueh <frank.hsueh@gmail.com>
2012-05-12 20:47:59 -0700

    WildcardQuery tests, adapted from TermQuery

---
+++ src/PlainElastic.Net.Tests/Builders/Queries/Wildcard/When_WildcardQuery_without_value_built.cs
@@ -0,0 +1,18 @@
+﻿using Machine.Specifications;
+using PlainElastic.Net.Queries;
+
+namespace PlainElastic.Net.Tests.Builders.Queries
+{
+    [Subject(typeof(WildcardQuery<>))]
+    class When_WildcardQuery_without_value_built
+    {
+        Because of = () => result = new WildcardQuery<FieldsTestClass>()
+                                                .Field(f => f.StringProperty)
+                                                .Boost(5)
+                                                .ToString();
+
+        It should_return_empty_string = () => result.ShouldBeEmpty();
+
+        private static string result;
+    }
+}
---
+++ src/PlainElastic.Net.Tests/Builders/Queries/Wildcard/When_complete_WildcardQuery_built.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+using PlainElastic.Net.Queries;
+using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net.Tests.Builders.Queries
+{
+    [Subject(typeof(WildcardQuery<>))]
+    class When_complete_WildcardQuery_built
+    {
+        Because of = () => result = new WildcardQuery<FieldsTestClass>()
+                                                .Field(f => f.StringProperty)
+                                                .Value("One")
+                                                .Boost(5)
+                                                .ToString();
+
+        It should_contain_boost_part = () => result.ShouldContain(@"'boost': 5".AltQuote());
+
+        It should_return_correct_query = () => result.ShouldEqual(@"{ 'term': { 'StringProperty': { 'value': 'One','boost': 5 } } }".AltQuote());
+
+        private static string result;
+    }
+}

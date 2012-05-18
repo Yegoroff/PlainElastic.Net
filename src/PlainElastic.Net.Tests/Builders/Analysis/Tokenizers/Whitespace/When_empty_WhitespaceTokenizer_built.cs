d9c4c10ec0ab82d2a3a8e67c4be4bef9d7defa79
Andriy Kozachuk <dev.oyster@gmail.com>
2012-05-18 12:37:59 +0300

    Added Letter, Lowercase and Whitespace tokenizer builders

---
+++ src/PlainElastic.Net.Tests/Builders/Analysis/Tokenizers/Whitespace/When_empty_WhitespaceTokenizer_built.cs
@@ -0,0 +1,18 @@
+﻿using Machine.Specifications;
+using PlainElastic.Net.IndexSettings;
+using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net.Tests.Builders.IndexSettings
+{
+    [Subject(typeof(WhitespaceTokenizer))]
+    class When_empty_WhitespaceTokenizer_built
+    {
+        Because of = () => result = new WhitespaceTokenizer()
+                                            .Name("name")
+                                            .ToString();
+
+        It should_return_correct_result = () => result.ShouldEqual("'name': { 'type': 'whitespace' }".AltQuote());
+
+        private static string result;
+    }
+}

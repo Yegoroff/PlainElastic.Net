27059f583d95d17948943f982c5c46467769564b
Vitaliy Litovskiy <vitaliy.litovskiy@gmail.com>
2012-07-29 01:23:00 +0300

    added support for Status command

---
+++ src/PlainElastic.Net/Builders/Commands/StatusCommand.cs
@@ -0,0 +1,25 @@
+﻿using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net
+{
+	/// <summary>
+	/// Builds a command that allows to get a comprehensive status information of one or more indices.
+	/// http://www.elasticsearch.org/guide/reference/api/admin-indices-status.html
+	/// </summary>
+	public class StatusCommand : CommandBuilder<StatusCommand>
+	{
+		public string Index { get; private set; }
+
+		public StatusCommand(string index = null)
+		{
+			Index = index;
+		}
+
+		protected override string BuildUrlPath()
+		{
+			return UrlBuilder.BuildUrlPath(Index, "_status");
+		}
+	}
+
+
+}

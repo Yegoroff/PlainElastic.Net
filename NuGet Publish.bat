f123de278f99bd99d09b67160c674457f97ec9d8
ayrov <ayroff@gmail.com>
2013-06-18 22:34:23 +0300

    Changed version to 1.0.36

--- NuGet Publish.bat
+++ NuGet Publish.bat
@@ -2,7 +2,7 @@
 
 @echo ==========================
 @echo NuGet package publishing.
-@%NUGET% Push NuGet\PlainElastic.Net.1.0.35.nupkg
+@%NUGET% Push NuGet\PlainElastic.Net.1.0.36.nupkg
 @if not errorlevel 0 goto error
 
 @echo PlainElastic.Net publishing sucessfull.

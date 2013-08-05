a18be9fc4049f7c0bedb682e16a42c688225f21c
tombuildsstuff <tharvey@opentable.com>
2013-08-05 11:31:09 +0100

    Added support for elastic search to have settings; i.e. the number of replicas and shards

--- src/PlainElastic.Net.Tests/Builders/IndexSettings/When_complete_IndexSettingsBuilder_built.cs
+++ src/PlainElastic.Net.Tests/Builders/IndexSettings/When_complete_IndexSettingsBuilder_built.cs
@@ -9,12 +9,14 @@ namespace PlainElastic.Net.Tests.Builders.IndexSettings
     {
         Because of = () => result = new IndexSettingsBuilder()
                                             .Analysis(a => a.CustomPart("Analysis"))
+                                            .Settings(a => a.CustomPart("Settings"))
                                             .Build();
 
         It should_contain_analysis_part = () => result.ShouldContain("'analysis': { Analysis }".AltQuote());
-        
-        It should_return_correct_result = () => result.ShouldEqual(("{ 'index': { " +
-                                                                    "'analysis': { Analysis } } }").AltQuote());
+
+        It should_contain_settings_part = () => result.ShouldContain("Settings");
+
+        It should_return_correct_result = () => result.ShouldEqual(("{ 'settings': { 'index': { 'analysis': { Analysis },Settings } } }").AltQuote());
 
         private static string result;
     }
---
+++ src/PlainElastic.Net.Tests/Builders/Settings/When_settings_builder_is_empty.cs
@@ -0,0 +1,17 @@
+﻿using Machine.Specifications;
+
+using PlainElastic.Net.Builders.IndexSettings.Settings;
+using PlainElastic.Net.Utils;
+
+namespace PlainElastic.Net.Tests.Builders.Settings
+{
+    [Subject(typeof(Setting))]
+    public class When_settings_builder_is_empty
+    {
+        private static string result;
+
+        Because of = () => result = new Setting().ToString();
+
+        private It should_contain_no_settings_part = () => result.ShouldContain("".AltQuote());
+    }
+}

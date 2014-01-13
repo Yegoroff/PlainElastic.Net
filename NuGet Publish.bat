217955a0216796778986b5c7f0e54128edbf1ee5
ayrov <ayroff@gmail.com>
2014-01-13 18:42:20 +0200

    Changed version to 1.0.45

--- NuGet Publish.bat
+++ NuGet Publish.bat
@@ -2,7 +2,7 @@
 
 @echo ==========================
 @echo NuGet package publishing.
-@%NUGET% Push NuGet\PlainElastic.Net.1.0.44.nupkg
+@%NUGET% Push NuGet\PlainElastic.Net.1.0.45.nupkg
 @if not errorlevel 0 goto errors
 
 @echo PlainElastic.Net publishing sucessfull.
--- src/PlainElastic.Net/Properties/AssemblyInfo.cs
+++ src/PlainElastic.Net/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PlainElastic.Net")]
 [assembly: AssemblyDescription("Plain ElasticSearch .Net client.")]
 [assembly: AssemblyProduct("PlainElastic.Net")]
-[assembly: AssemblyCopyright("Copyright © Alexander Yegorov 2013")]
+[assembly: AssemblyCopyright("Copyright © Alexander Yegorov 2014")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("e7493a69-683e-45b3-be08-4f5ce80508c8")]
 
-[assembly: AssemblyVersion("1.0.44.0")]
-[assembly: AssemblyFileVersion("1.0.44.0")]
+[assembly: AssemblyVersion("1.0.45.0")]
+[assembly: AssemblyFileVersion("1.0.45.0")]

22e39b2566ed2e6b891870673c5229c881de8be0
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-11-14 14:41:04 +0100

    release 1.0.48.1

--- src/PlainElastic.Net/Properties/AssemblyInfo.cs
+++ src/PlainElastic.Net/Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PlainElastic.Net")]
 [assembly: AssemblyDescription("Plain ElasticSearch .Net client.")]
-[assembly: AssemblyProduct("PlainElastic.Net")]
+[assembly: AssemblyProduct("PlainElastic.Net.Patched")]
 [assembly: AssemblyCopyright("Copyright © Alexander Yegorov 2014")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("e7493a69-683e-45b3-be08-4f5ce80508c8")]
 
-[assembly: AssemblyVersion("1.0.48.0")]
-[assembly: AssemblyFileVersion("1.0.48.0")]
+[assembly: AssemblyVersion("1.0.48.1")]
+[assembly: AssemblyFileVersion("1.0.48.1")]
